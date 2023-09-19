75800377a62cb89095ae97d2f5ceb0dcf4e6181a
erichuang11 <eric.h@gokabu.ca>
2023-09-19 11:54:56 -0700

    add project display

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as  Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+import ProjectDisplay from './pages/ProjectDisplay';
 import Experience from './pages/Experience';
 import More from './pages/More';
 import Navbar from './components/Navbar';
@@ -15,8 +16,10 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/projects' element={<Projects />} />
+          <Route path='/project/:id' element={<ProjectDisplay />}/>
           <Route path='/Experience' element={<Experience />} />
           <Route path='/More' element={<More />} />
+
         </Routes>
         <Footer />
       </Router>
